37f5bc7fad8b16f019eb5f066274fa1c6a30224e
Fady Nabil <68451496+fadyNabil16@users.noreply.github.com>
2024-11-11 18:07:21 +0200

    edit create file

--- createFiles.py
+++ createFiles.py
@@ -2,14 +2,18 @@ import os
 
 
 class CreateFile:
-    def create():
+    def create() -> None:
+        """ 
+        create function is used to create automatically Db file if its Not created 
+        """
         if not os.path.exists("market1.db"):
             try:
-                f = open("market1.db", "x")
-            except Exception:
-                pass
+                f = open("market1.db", mode="x")
+            except TypeError as err:
+                print(err)
+
         if not os.path.exists("market2.db"):
             try:
-                f = open("market2.db", "x")
+                f = open("market2.db", "rx")
             except os.error:
                 pass
